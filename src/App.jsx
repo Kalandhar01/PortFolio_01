a2407e795cd09ba66a1b2a43d5ee03af6b3b0265
KALANDHAR <kalandars2004@gmail.com>
2024-09-24 23:52:14 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { Suspense, lazy } from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
 import Nav from './component/Nav';
+import PreLoader from './component/PreLoader';
 
 // Lazy loading components
 const About = lazy(() => import('./router/About'));
@@ -10,36 +11,52 @@ const Why = lazy(() => import('./router/Why'));
 const Skill = lazy(() => import('./router/Skill'));
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);  
+    }, 4000);
+
+    return () => clearTimeout(timer); 
+  }, []);
+
   return (
     <>
-      <Nav />
+      {loading ? (
+       
+        <PreLoader />
+      ) : (
+        <>
+          <Nav />
+          <Suspense fallback={<div>Loading...</div>}>
+            <div id="landing-page">
+              <LandingPage />
+            </div>
 
-      <Suspense fallback={<div>Loading...</div>}>   
-        <div id="landing-page">
-          <LandingPage />
-        </div>
+            <div id="about">
+              <About />
+            </div>
 
-        {/* Only load About component when needed */}
-        <div id="about">
-          <About />
-        </div>
+            <div id="why">
+              <Why />
+            </div>
 
-        <div id="why">
-          <Why />
-        </div>
+            <div id="skills">
+              <Skill />
+            </div>
 
-        <div id="skills">
-          <Skill />
-        </div>
+            <div id="contact">
+              <Contact />
+            </div>
 
-        <div id="contact">
-          <Contact />
-        </div>
-
-        <div id="blog">
-          <Blog />
-        </div>
-      </Suspense>
+            <div id="blog">
+              <Blog />
+            </div>
+          </Suspense>
+        </>
+      )}
     </>
   );
 };
